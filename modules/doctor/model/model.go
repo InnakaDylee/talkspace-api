f46de0e80b35125c13f02b1398ea97ec00065b58
Hanisah Fildza Annafisah <hanisahfildza@gmail.com>
2024-08-14 23:02:04 +0700

    feature-doctor: update model

--- modules/doctor/model/model.go
+++ modules/doctor/model/model.go
@@ -7,25 +7,25 @@ import (
 )
 
 type Doctor struct {
-	ID             string  `gorm:"primarykey"`
-	Fullname       string  `gorm:"not null"`
-	Email          string  `gorm:"not null"`
-	Password       string  `gorm:"not null"`
-	ProfilePicture string  `gorm:"not null"`
-	Status         bool    `gorm:"not null;default:false"`
-	Gender         *string `gorm:"type:gender;default:NULL"`
-	Specialist     string  `gorm:"not null"`
-	Experience     string  `gorm:"not null"`
-	Price          float64 `gorm:"not null"`
-	StrNumber      string  `gorm:"not null"`
-	Alumnus        string  `gorm:"not null"`
-	About          string  `gorm:"not null"`
-	Location       string  `gorm:"not null"`
-	Role           string  `gorm:"type:role;default:'doctor'"`
-	OTP            string  `gorm:"not null"`
-	OTPExpiration  int64
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      *time.Time       `gorm:"index"`
-	Transaction    []tm.Transaction `gorm:"foreignKey:DoctorID"`
+	ID                string  `gorm:"primarykey"`
+	Fullname          string  `gorm:"not null"`
+	Email             string  `gorm:"not null"`
+	Password          string  `gorm:"not null"`
+	ProfilePicture    string  `gorm:"not null"`
+	Status            bool    `gorm:"not null;default:false"`
+	Gender            *string `gorm:"type:gender;default:NULL"`
+	Specialization    string  `gorm:"not null"`
+	YearsOfExperience string  `gorm:"not null"`
+	Price             float64 `gorm:"not null"`
+	LicenseNumber     string  `gorm:"not null"`
+	Alumnus           string  `gorm:"not null"`
+	About             string  `gorm:"not null"`
+	Location          string  `gorm:"not null"`
+	Role              string  `gorm:"type:role;default:'doctor'"`
+	OTP               string  `gorm:"not null"`
+	OTPExpiration     int64
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         *time.Time       `gorm:"index"`
+	Transaction       []tm.Transaction `gorm:"foreignKey:DoctorID"`
 }
